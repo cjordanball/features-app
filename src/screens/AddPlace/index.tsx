6cb62c20bd5b6a8bf7a416a15f75e2be6e8f31da
Jordan Ball <cjordanball@ballwebdev.com>
2023-08-04 08:12:00 -0400

    add sql functions to database

--- src/screens/AddPlace/index.tsx
+++ src/screens/AddPlace/index.tsx
@@ -2,12 +2,13 @@ import { Text, View } from 'react-native';
 import { styles } from './addPlaceStyle';
 import { PlaceForm } from '@/components';
 import { Place } from '@/types';
+import { insertPlace } from '@/utilities/database';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 const AddPlace = ({ navigation: nav }) => {
-	const createPlaceHandler = (place: Place) => {
-		nav.navigate('AllPlaces', {
-			place: place,
-		});
+	const createPlaceHandler = async (place: Place) => {
+		await insertPlace(place);
+		nav.navigate('AllPlaces');
 	};
 
 	return <PlaceForm onCreatePlace={createPlaceHandler} />;
